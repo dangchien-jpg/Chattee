5038d0b5673709275ff15377e8afbc9c53838fc5
dangchien-jpg <nguyendangchien12.5.a@gmail.com>
2026-04-08 22:38:23 +0700

    fix(UI): add backgroud gradient for sign in, sign up form

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -20,7 +20,7 @@ const SignInPage = () => {
     }
   }, [accessToken, user]);
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center p-6  md:p-10">
+    <div className="flex min-h-svh flex-col items-center justify-center p-6  md:p-10 bg-gradient-purple">
       <div className="w-full md:max-w-3xl ">
         <SignInForm />
       </div>
--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -2,7 +2,7 @@ import { SignupForm } from "@/components/auth/signup-form";
 
 const SignUpPage = () => {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center p-6  md:p-10">
+    <div className="flex min-h-svh flex-col items-center justify-center p-6  md:p-10 bg-gradient-purple">
       <div className="w-full md:max-w-3xl ">
         <SignupForm />
       </div>

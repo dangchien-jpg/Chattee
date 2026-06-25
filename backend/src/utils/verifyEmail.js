c33bc382d0c881ec2d343c508e4c95092fa985e5
dangchien-jpg <nguyendangchien12.5.a@gmail.com>
2026-06-25 21:57:00 +0700

    fix(auth): change gmail to smtp.gmail.com for production

--- backend/src/utils/verifyEmail.js
+++ backend/src/utils/verifyEmail.js
@@ -3,7 +3,9 @@ import nodemailer from "nodemailer";
 export const sendVerifyEmail = async (token, email) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASSWORD,
